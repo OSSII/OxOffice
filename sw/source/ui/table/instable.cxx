LibreOffice/core

sw: change inserttable style option default to 1
Right now it is default to NONE in the list
if the user explicitly choses otherwise but that
does not align with inserttable option on the toolbar
there it is defaulted to "Default Table Style"
1 means "Default Table Style"

Signed-off-by: Mert Tumer <mert.tumer@collabora.com>
Change-Id: I1db19f0292ac6775653b0db3f2860fea9e3b0adf

sw/source/ui/table/instable.cxx
@@ -149,9 +149,8 @@ void SwInsTableDlg::InitAutoTableFormat()
     // Change this min variable if you add autotable manually.
     minTableIndexInLb = 1;
     maxTableIndexInLb = minTableIndexInLb + static_cast<sal_uInt8>(pTableTable->size());
-    lbIndex = 0;
-    m_xLbFormat->select( lbIndex );
-    tbIndex = lbIndexToTableIndex(lbIndex);
+    m_xLbFormat->select( minTableIndexInLb );
+    tbIndex = lbIndexToTableIndex( minTableIndexInLb );
 
     SelFormatHdl( *m_xLbFormat );
 }

OSSII/OxOffice

Reviewed-on: https://gerrit.libreoffice.org/c/core/+/131971
Tested-by: Andras Timar <andras.timar@collabora.com>
Reviewed-by: Andras Timar <andras.timar@collabora.com>
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/146722
Tested-by: Szymon Kłos <szymon.klos@collabora.com>
Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
(cherry picked from commit ef46afe71751929b8b17d278d83c8e3ceefc862f)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/146701

sw/source/ui/table/instable.cxx
@@ -148,9 +148,8 @@ void SwInsTableDlg::InitAutoTableFormat()
     // Change this min variable if you add autotable manually.
     minTableIndexInLb = 1;
     maxTableIndexInLb = minTableIndexInLb + static_cast<sal_uInt8>(m_xTableTable->size());
-    m_lbIndex = 0;
-    m_xLbFormat->select( m_lbIndex );
-    m_tbIndex = lbIndexToTableIndex(m_lbIndex);
+    m_xLbFormat->select( minTableIndexInLb );
+    m_tbIndex = lbIndexToTableIndex( minTableIndexInLb );
 
     SelFormatHdl( *m_xLbFormat );
 }