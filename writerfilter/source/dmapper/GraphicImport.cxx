LibreOffice/core

tdf#143311 writerfilter: import decorative flag on as-char flys too
Change-Id: I9704d35a8286b92169dffd8de7e57e286884f607
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/148016
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

writerfilter/source/dmapper/GraphicImport.cxx
@@ -1795,10 +1795,10 @@ uno::Reference<text::XTextContent> GraphicImport::createGraphicObject(uno::Refer
                 xGraphicObjectProperties->setPropertyValue(getPropertyName( PROP_SIZE_PROTECTED ),
                     uno::Any(true));
 
+            xGraphicObjectProperties->setPropertyValue(getPropertyName(PROP_DECORATIVE), uno::Any(m_pImpl->m_bDecorative));
             sal_Int32 nWidth = - m_pImpl->m_nLeftPosition;
             if (m_pImpl->m_rGraphicImportType == IMPORT_AS_DETECTED_ANCHOR)
             {
-                xGraphicObjectProperties->setPropertyValue(getPropertyName(PROP_DECORATIVE), uno::Any(m_pImpl->m_bDecorative));
                 //adjust margins
                 if( (m_pImpl->m_nHoriOrient == text::HoriOrientation::LEFT &&
                     (m_pImpl->m_nHoriRelation == text::RelOrientation::PAGE_PRINT_AREA ||

OSSII/OxOffice

(cherry picked from commit d4355f14d1483a57fa08579b143a762f0aad4631)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/148019
Reviewed-by: Caolán McNamara <caolanm@redhat.com>

writerfilter/source/dmapper/GraphicImport.cxx
@@ -1792,10 +1792,10 @@ uno::Reference<text::XTextContent> GraphicImport::createGraphicObject(uno::Refer
                 xGraphicObjectProperties->setPropertyValue(getPropertyName( PROP_SIZE_PROTECTED ),
                     uno::Any(true));
 
+            xGraphicObjectProperties->setPropertyValue(getPropertyName(PROP_DECORATIVE), uno::Any(m_pImpl->bDecorative));
             sal_Int32 nWidth = - m_pImpl->nLeftPosition;
             if (m_pImpl->m_rGraphicImportType == IMPORT_AS_DETECTED_ANCHOR)
             {
-                xGraphicObjectProperties->setPropertyValue(getPropertyName(PROP_DECORATIVE), uno::Any(m_pImpl->bDecorative));
                 //adjust margins
                 if( (m_pImpl->nHoriOrient == text::HoriOrientation::LEFT &&
                     (m_pImpl->nHoriRelation == text::RelOrientation::PAGE_PRINT_AREA ||