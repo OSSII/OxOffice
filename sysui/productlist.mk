4cf423ef074d5ea56f3a558963828ff6f4a082f3
Firefly <firefly@ossii.com.tw>
2023-05-10 17:08:22 +0800

    Change the package name to OxOffice.
    And the initial version number is set to 10.0.1.0
    
    Change-Id: I687f4d2b6ae2d4effc7561ed5a823de961230d80

--- sysui/productlist.mk
+++ sysui/productlist.mk
@@ -7,12 +7,14 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-PRODUCTLIST := libreoffice libreofficedev
+PRODUCTLIST := oxoffice
 PKGVERSION := $(LIBO_VERSION_MAJOR).$(LIBO_VERSION_MINOR).$(LIBO_VERSION_MICRO)
 PKGVERSIONSHORT := $(LIBO_VERSION_MAJOR).$(LIBO_VERSION_MINOR)
 PRODUCTNAME.libreoffice := LibreOffice
 PRODUCTNAME.libreofficedev := LibreOfficeDev
+PRODUCTNAME.oxoffice := OxOffice
 UNIXFILENAME.libreoffice := libreoffice$(PKGVERSIONSHORT)
 UNIXFILENAME.libreofficedev := libreofficedev$(PKGVERSIONSHORT)
+UNIXFILENAME.oxoffice := oxoffice
 
 # vim: set noet sw=4 ts=4:
