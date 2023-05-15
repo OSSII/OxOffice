LibreOffice/core

tdf#153923: In ReqIF case, avoid fake dl/dd used to emulate indentation
Change-Id: Iac85350c8f52a39173a07131c67e7dc943aa140e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/150229
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

sw/source/filter/html/htmltabw.cxx
@@ -1024,7 +1024,8 @@ SwHTMLWriter& OutHTML_SwTableNode( SwHTMLWriter& rWrt, SwTableNode & rNode,
         break;
     }
 
-    if( bCheckDefList )
+    // In ReqIF case, do not emulate indentation with fake description list
+    if( bCheckDefList && !rWrt.mbReqIF )
     {
         OSL_ENSURE( !rWrt.GetNumInfo().GetNumRule() ||
                 rWrt.GetNextNumInfo(),

OSSII/OxOffice

(cherry picked from commit 3ce1616ea371e0efd317290bb4f943f319404800)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/150200
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

sw/source/filter/html/htmltabw.cxx
@@ -1026,7 +1026,8 @@ Writer& OutHTML_SwTableNode( Writer& rWrt, SwTableNode & rNode,
         break;
     }
 
-    if( bCheckDefList )
+    // In ReqIF case, do not emulate indentation with fake description list
+    if( bCheckDefList && !rHTMLWrt.mbReqIF )
     {
         OSL_ENSURE( !rHTMLWrt.GetNumInfo().GetNumRule() ||
                 rHTMLWrt.GetNextNumInfo(),