7c664db246148ce52618571c09feb0db6fd401d4
Jeff Huang <jeff.huang@ossii.com.tw>
2023-11-13 09:39:30 +0800

    Revert "Fix windows build error C2672 'std::min'"
    This reverts commit 0b48af88dea28ae86c47c67c58d7a32d5c50b8c7.

--- sw/source/core/txtnode/fntcache.cxx
+++ sw/source/core/txtnode/fntcache.cxx
@@ -537,8 +537,8 @@ void SwFntObj::GuessLeading( const SwViewShell&
                 // Those who lie about their Leading, may lie about their
                 // Ascent/Descent as well, hence the Font will be lowered a
                 // little without changing its height.
-                sal_Int32 nDiff = std::min( (sal_Int32)(rMet.GetDescent() - aWinMet.GetDescent()),
-                    (sal_Int32)(aWinMet.GetAscent() - rMet.GetAscent() - nTmpLeading) );
+                sal_Int32 nDiff = std::min( rMet.GetDescent() - aWinMet.GetDescent(),
+                    aWinMet.GetAscent() - rMet.GetAscent() - nTmpLeading );
                 if( nDiff > 0 )
                 {
                     OSL_ENSURE( m_nPrtAscent < USHRT_MAX, "GuessLeading: PrtAscent-Fault" );
