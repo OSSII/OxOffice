LibreOffice/core

tdf#159107 sw floattable: prefer inline table in footnote with change tracking
Writer doesn't really support tables in footnotes, see the warning at
the top of SwFlowFrame::MoveFwd(). Still, there is some initial support
for them, since commit 11c51eefe8c3210cef2b5850f401ba67a401d01
(tdf#95806 tdf#125877 tdf#141172 DOCX: fix tables in footnotes,
2021-03-28).  Similarly, redlines really just track insertion and
deletion, but some initial support for table operations were added in
commit eebe4747d2d13545004937bb0267ccfc8ab9d63f (tdf#144270 sw: manage
tracked table (row) deletion/insertion, 2022-01-12).

The combination of these is a bit fragile, but the bugdoc happened to be
imported as an inline table (in a footnote, with recording changes)
before commit d477fa8ac1b0d3ee81427217bbb5950278ab16db (sw floattable:
unconditionally map <w:tblpPr> to SwFormatFlySplit, 2023-03-17).

Fix the problem by explicitly importing floating tables as inline in the
footnote + redline case to restore the working use-case.

DOCX import of this combination can be enabled again once Writer layout
works for the produced model.

Change-Id: I9cd0a1fabb9807f5117cb5a36ec8597d6646eece
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162733
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

writerfilter/qa/cppunittests/dmapper/DomainMapperTableHandler.cxx
@@ -183,6 +183,20 @@ CPPUNIT_TEST_FIXTURE(Test, testDOCXFloatingTableHeader)
     // 2233 pages and then there was a layout loop.
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(2), nLastPage);
 }
+
+CPPUNIT_TEST_FIXTURE(Test, testDOCXFloatingTableFootnoteRedline)
+{
+    // Given a document with a floating table in a footnote, with track changes recorded (but not
+    // visible):
+    // When importing that document:
+    loadFromFile(u"floattable-footnote-redline.docx");
+
+    // Then make sure the table is imported as inline, till Writer layout is ready to handle
+    // floating tables in footnotes:
+    uno::Reference<drawing::XDrawPageSupplier> xModel(mxComponent, uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage = xModel->getDrawPage();
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0), xDrawPage->getCount());
+}
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();

OSSII/OxOffice

(cherry picked from commit af15f8b7f346898677f1eee6521a6be1ff63eb56)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/162716
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163297
(cherry picked from commit 9589438d53f32ba7b59512d56a6dbd1d28de70a6)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/164071
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

writerfilter/qa/cppunittests/dmapper/DomainMapperTableHandler.cxx
@@ -183,6 +183,20 @@ CPPUNIT_TEST_FIXTURE(Test, testDOCXFloatingTableHeader)
     // 2233 pages and then there was a layout loop.
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(2), nLastPage);
 }
+
+CPPUNIT_TEST_FIXTURE(Test, testDOCXFloatingTableFootnoteRedline)
+{
+    // Given a document with a floating table in a footnote, with track changes recorded (but not
+    // visible):
+    // When importing that document:
+    loadFromURL(u"floattable-footnote-redline.docx");
+
+    // Then make sure the table is imported as inline, till Writer layout is ready to handle
+    // floating tables in footnotes:
+    uno::Reference<drawing::XDrawPageSupplier> xModel(mxComponent, uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage = xModel->getDrawPage();
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0), xDrawPage->getCount());
+}
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();