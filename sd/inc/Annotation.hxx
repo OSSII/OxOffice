LibreOffice/core

annot: added Annotation impl. to svx, moved some thing from sd
This is an attempt to move the sd::Annotation to the common code
in the svx module. This will need to be done in multiple steps so
the first one is to introduce sdr::annotation::Annotation class in
svx module, which is derived by sd::Annotation. Non-problematic
functionality and members are also moved to the svx Annotation.

Change-Id: Id20466b3780514ab63a9df8923b879098870ebb6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166492
Tested-by: Jenkins
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

sd/inc/Annotation.hxx
@@ -26,6 +26,7 @@
 #include <com/sun/star/office/XAnnotation.hpp>
 #include <comphelper/compbase.hxx>
 #include <cppuhelper/propertysetmixin.hxx>
+#include <svx/annotation/Annotation.hxx>
 
 #include "sdpage.hxx"
 #include "textapi.hxx"
@@ -67,19 +68,16 @@ struct SD_DLLPUBLIC CustomAnnotationMarker
 };
 
 class SAL_DLLPUBLIC_RTTI Annotation final :
-                   public ::comphelper::WeakComponentImplHelper<css::office::XAnnotation>,
-                   public ::cppu::PropertySetMixin<css::office::XAnnotation>
+                    public sdr::annotation::Annotation,
+                    public ::comphelper::WeakComponentImplHelper<css::office::XAnnotation>,
+                     public ::cppu::PropertySetMixin<css::office::XAnnotation>
 {
 public:
     explicit Annotation( const css::uno::Reference<css::uno::XComponentContext>& context, SdPage* pPage );
     Annotation(const Annotation&) = delete;
     Annotation& operator=(const Annotation&) = delete;
 
-    static sal_uInt32 m_nLastId;
-
     SdPage* GetPage() const { return mpPage; }
-    SdrModel* GetModel() { return (mpPage != nullptr) ? &mpPage->getSdrModelFromSdrPage() : nullptr; }
-    sal_uInt32 GetId() const { return m_nId; }
 
     // XInterface:
     virtual css::uno::Any SAL_CALL queryInterface(css::uno::Type const & type) override;
@@ -141,17 +139,9 @@ private:
 
     void createChangeUndoImpl(std::unique_lock<std::mutex>& g);
 
-    sal_uInt32 m_nId;
     SdPage* mpPage;
-    css::geometry::RealPoint2D m_Position;
-    css::geometry::RealSize2D m_Size;
-    OUString m_Author;
-    OUString m_Initials;
-    css::util::DateTime m_DateTime;
     rtl::Reference<TextApiObject> m_TextRange;
-
     std::unique_ptr<CustomAnnotationMarker> m_pCustomAnnotationMarker;
-    bool m_bIsFreeText;
 };
 
 }

OSSII/OxOffice

(cherry picked from commit a938ed2be520426ce7949c4fd30a6e7e31d7c279)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/168419
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sd/inc/Annotation.hxx
@@ -27,6 +27,7 @@
 #include <cppuhelper/basemutex.hxx>
 #include <cppuhelper/compbase.hxx>
 #include <cppuhelper/propertysetmixin.hxx>
+#include <svx/annotation/Annotation.hxx>
 
 #include "sdpage.hxx"
 #include "textapi.hxx"
@@ -74,19 +75,16 @@ struct SD_DLLPUBLIC CustomAnnotationMarker
 };
 
 class SD_DLLPUBLIC Annotation final : private ::cppu::BaseMutex,
-                   public ::cppu::WeakComponentImplHelper<css::office::XAnnotation>,
-                   public ::cppu::PropertySetMixin<css::office::XAnnotation>
+                    public sdr::annotation::Annotation,
+                    public ::cppu::WeakComponentImplHelper<css::office::XAnnotation>,
+                    public ::cppu::PropertySetMixin<css::office::XAnnotation>
 {
 public:
     explicit Annotation( const css::uno::Reference<css::uno::XComponentContext>& context, SdPage* pPage );
     Annotation(const Annotation&) = delete;
     Annotation& operator=(const Annotation&) = delete;
 
-    static sal_uInt32 m_nLastId;
-
     SdPage* GetPage() const { return mpPage; }
-    SdrModel* GetModel() { return (mpPage != nullptr) ? &mpPage->getSdrModelFromSdrPage() : nullptr; }
-    sal_uInt32 GetId() const { return m_nId; }
 
     // XInterface:
     virtual css::uno::Any SAL_CALL queryInterface(css::uno::Type const & type) override;
@@ -146,17 +144,9 @@ private:
     // disposed, do it here.
     virtual void SAL_CALL disposing() override;
 
-    sal_uInt32 m_nId;
     SdPage* mpPage;
-    css::geometry::RealPoint2D m_Position;
-    css::geometry::RealSize2D m_Size;
-    OUString m_Author;
-    OUString m_Initials;
-    css::util::DateTime m_DateTime;
     rtl::Reference<TextApiObject> m_TextRange;
-
     std::unique_ptr<CustomAnnotationMarker> m_pCustomAnnotationMarker;
-    bool m_bIsFreeText;
 };
 
 }