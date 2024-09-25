LibreOffice/core

annot: moved more of Annotation and dependencies into svx
Moved the holder of annotations from SdPage to SvxPage, so that
the vector holding the annotations and accessors are on SvxPage
and adapted the code.

This also changes the type od most parameters on most methods
from sd::Annotation to sdr::annotation::Annotation and adapted
the code.

Moved AnnotationEnumeration into svx, as it was needed in svx
already.

Change-Id: Iab17aa881443f58adfb9158959db00ed24076279
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166494
Tested-by: Jenkins
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

sd/inc/Annotation.hxx
@@ -46,9 +46,9 @@ class SfxViewShell;
 namespace sd
 {
 
-void createAnnotation( rtl::Reference< Annotation >& xAnnotation, SdPage* pPage );
+void createAnnotation(rtl::Reference<sdr::annotation::Annotation>& xAnnotation, SdPage* pPage);
 
-std::unique_ptr<SdrUndoAction> CreateUndoInsertOrRemoveAnnotation( const rtl::Reference< sd::Annotation >& xAnnotation, bool bInsert );
+std::unique_ptr<SdrUndoAction> CreateUndoInsertOrRemoveAnnotation(rtl::Reference<sdr::annotation::Annotation>& xAnnotation, bool bInsert);
 
 struct SD_DLLPUBLIC CustomAnnotationMarker
 {
@@ -58,22 +58,14 @@ struct SD_DLLPUBLIC CustomAnnotationMarker
     std::vector<basegfx::B2DPolygon> maPolygons;
 };
 
-class SAL_DLLPUBLIC_RTTI Annotation final :
-                    public sdr::annotation::Annotation,
-                    public ::comphelper::WeakComponentImplHelper<css::office::XAnnotation>,
-                     public ::cppu::PropertySetMixin<css::office::XAnnotation>
+class SAL_DLLPUBLIC_RTTI Annotation final : public sdr::annotation::Annotation
 {
 public:
     explicit Annotation( const css::uno::Reference<css::uno::XComponentContext>& context, SdPage* pPage );
     Annotation(const Annotation&) = delete;
     Annotation& operator=(const Annotation&) = delete;
 
-    SdPage* GetPage() const { return mpPage; }
-
-    // XInterface:
-    virtual css::uno::Any SAL_CALL queryInterface(css::uno::Type const & type) override;
-    virtual void SAL_CALL acquire() noexcept override { ::comphelper::WeakComponentImplHelper<css::office::XAnnotation>::acquire(); }
-    virtual void SAL_CALL release() noexcept override { ::comphelper::WeakComponentImplHelper<css::office::XAnnotation>::release(); }
+    virtual ~Annotation();
 
     // css::beans::XPropertySet:
     virtual css::uno::Reference<css::beans::XPropertySetInfo> SAL_CALL getPropertySetInfo() override;
@@ -129,7 +121,6 @@ private:
 
     void createChangeUndoImpl(std::unique_lock<std::mutex>& g);
 
-    SdPage* mpPage;
     rtl::Reference<TextApiObject> m_TextRange;
     std::unique_ptr<CustomAnnotationMarker> m_pCustomAnnotationMarker;
 };

OSSII/OxOffice

(cherry picked from commit a0a581ead18f030f59d203539706de0230746cae)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/168555
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

sd/inc/Annotation.hxx
@@ -24,8 +24,6 @@
 #include <memory>
 
 #include <com/sun/star/office/XAnnotation.hpp>
-#include <cppuhelper/basemutex.hxx>
-#include <cppuhelper/compbase.hxx>
 #include <cppuhelper/propertysetmixin.hxx>
 #include <svx/annotation/Annotation.hxx>
 
@@ -47,9 +45,9 @@ class SfxViewShell;
 namespace sd
 {
 
-void createAnnotation( rtl::Reference< Annotation >& xAnnotation, SdPage* pPage );
+void createAnnotation(rtl::Reference<sdr::annotation::Annotation>& xAnnotation, SdPage* pPage);
 
-std::unique_ptr<SdrUndoAction> CreateUndoInsertOrRemoveAnnotation( const css::uno::Reference< css::office::XAnnotation >& xAnnotation, bool bInsert );
+std::unique_ptr<SdrUndoAction> CreateUndoInsertOrRemoveAnnotation(rtl::Reference<sdr::annotation::Annotation>& xAnnotation, bool bInsert);
 
 struct SD_DLLPUBLIC CustomAnnotationMarker
 {
@@ -59,22 +57,12 @@ struct SD_DLLPUBLIC CustomAnnotationMarker
     std::vector<basegfx::B2DPolygon> maPolygons;
 };
 
-class SD_DLLPUBLIC Annotation final : private ::cppu::BaseMutex,
-                    public sdr::annotation::Annotation,
-                    public ::cppu::WeakComponentImplHelper<css::office::XAnnotation>,
-                    public ::cppu::PropertySetMixin<css::office::XAnnotation>
+class SD_DLLPUBLIC Annotation final : public sdr::annotation::Annotation
 {
 public:
     explicit Annotation( const css::uno::Reference<css::uno::XComponentContext>& context, SdPage* pPage );
     Annotation(const Annotation&) = delete;
     Annotation& operator=(const Annotation&) = delete;
-
-    SdPage* GetPage() const { return mpPage; }
-
-    // XInterface:
-    virtual css::uno::Any SAL_CALL queryInterface(css::uno::Type const & type) override;
-    virtual void SAL_CALL acquire() noexcept override { ::cppu::WeakComponentImplHelper<css::office::XAnnotation>::acquire(); }
-    virtual void SAL_CALL release() noexcept override { ::cppu::WeakComponentImplHelper<css::office::XAnnotation>::release(); }
 
     // css::beans::XPropertySet:
     virtual css::uno::Reference<css::beans::XPropertySetInfo> SAL_CALL getPropertySetInfo() override;
@@ -128,7 +116,6 @@ private:
     // disposed, do it here.
     virtual void SAL_CALL disposing() override;
 
-    SdPage* mpPage;
     rtl::Reference<TextApiObject> m_TextRange;
     std::unique_ptr<CustomAnnotationMarker> m_pCustomAnnotationMarker;
 };