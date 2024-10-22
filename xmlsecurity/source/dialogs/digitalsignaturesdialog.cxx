LibreOffice/core

cool#9992 lok doc sign: convert the certificate chooser dialog to async
1) Move the try-catch around assing a signature inside the block that
   already got the response from the certificate chooser, it's unlikely
   that choosing the certificate (and not yet signing) would fail.

2) Convert the dialog to async and allow it to be a jsdialog.

3) Hide not relevant widgets for LOK: we have a single signing
   certificate, so the search entry is not useful.

   The refresh button has an unclear purpose, as it was initially added
   in commit fb9874231fb242b4a6a7d1ce097e66dd4ed8f32e (Caching
   Certificates in the CertificateChooser dialog session-wise, 2023-08-26),
   but later commit efe414c4a88fde0e2ee1d4016010f21fbd6d01d7 (Don't reuse
   CertificateChooser instances, 2024-02-07) disabled this chaching. In any
   case, the certificate won't change during the lifetime of a single LOK
   view, so hide that as well.

4) Invoke the inner certificate viewer in an async way, too.

Change-Id: Ibf618ea7632cf801d1d9180b9aa7dd193c45ffda
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/174215
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

xmlsecurity/source/dialogs/digitalsignaturesdialog.cxx
@@ -482,27 +482,40 @@ IMPL_LINK_NOARG(DigitalSignaturesDialog, AddButtonHdl, weld::Button&, void)
 {
     if( ! canAdd())
         return;
-    try
-    {
-        std::vector<uno::Reference<xml::crypto::XXMLSecurityContext>> xSecContexts
-        {
-            maSignatureManager.getSecurityContext()
-        };
-        // Gpg signing is only possible with ODF >= 1.2 documents
-        if (DocumentSignatureHelper::CanSignWithGPG(maSignatureManager.getStore(), m_sODFVersion))
-            xSecContexts.push_back(maSignatureManager.getGpgSecurityContext());
 
-        std::unique_ptr<CertificateChooser> aChooser = CertificateChooser::getInstance(m_xDialog.get(), m_pViewShell, std::move(xSecContexts), CertificateChooserUserAction::Sign);
-        while (aChooser->run() == RET_OK)
+    // Separate function, so the function can call itself when the certificate choosing was
+    // successful, but the actual signing was not.
+    AddButtonHdlImpl();
+}
+
+void DigitalSignaturesDialog::AddButtonHdlImpl()
+{
+    std::vector<uno::Reference<xml::crypto::XXMLSecurityContext>> xSecContexts
+    {
+        maSignatureManager.getSecurityContext()
+    };
+    // Gpg signing is only possible with ODF >= 1.2 documents
+    if (DocumentSignatureHelper::CanSignWithGPG(maSignatureManager.getStore(), m_sODFVersion))
+        xSecContexts.push_back(maSignatureManager.getGpgSecurityContext());
+
+    std::shared_ptr<CertificateChooser> aChooser = CertificateChooser::getInstance(m_xDialog.get(), m_pViewShell, std::move(xSecContexts), CertificateChooserUserAction::Sign);
+    aChooser->BeforeRun();
+    weld::DialogController::runAsync(aChooser, [this, aChooser](sal_Int32 nRet) {
+        if (nRet != RET_OK)
+        {
+            return;
+        }
+
+        try
         {
             sal_Int32 nSecurityId;
 
             if (moScriptSignatureManager)
             {
                 if (!moScriptSignatureManager->add(aChooser->GetSelectedCertificates()[0],
-                                                   aChooser->GetSelectedSecurityContext(),
-                                                   aChooser->GetDescription(), nSecurityId,
-                                                   m_bAdESCompliant))
+                            aChooser->GetSelectedSecurityContext(),
+                            aChooser->GetDescription(), nSecurityId,
+                            m_bAdESCompliant))
                 {
                     return;
                 }
@@ -514,7 +527,7 @@ IMPL_LINK_NOARG(DigitalSignaturesDialog, AddButtonHdl, weld::Button&, void)
             }
 
             if (!maSignatureManager.add(aChooser->GetSelectedCertificates()[0], aChooser->GetSelectedSecurityContext(),
-                                        aChooser->GetDescription(), nSecurityId, m_bAdESCompliant))
+                        aChooser->GetDescription(), nSecurityId, m_bAdESCompliant))
                 return;
             mbSignaturesChanged = true;
 
@@ -535,21 +548,24 @@ IMPL_LINK_NOARG(DigitalSignaturesDialog, AddButtonHdl, weld::Button&, void)
                 mbVerifySignatures = true;
                 ImplGetSignatureInformations(/*bUseTempStream=*/true, /*bCacheLastSignature=*/false);
                 ImplFillSignaturesBox();
-                break;
+            }
+            else
+            {
+                AddButtonHdlImpl();
             }
         }
-    }
-    catch ( uno::Exception& )
-    {
-        TOOLS_WARN_EXCEPTION( "xmlsecurity.dialogs", "adding a signature!" );
-        std::unique_ptr<weld::MessageDialog> xBox(Application::CreateMessageDialog(m_xDialog.get(),
-                                                  VclMessageType::Error, VclButtonsType::Ok,
-                                                  XsResId(STR_XMLSECDLG_SIGNING_FAILED)));
-        xBox->run();
-        // Don't keep invalid entries...
-        ImplGetSignatureInformations(/*bUseTempStream=*/true, /*bCacheLastSignature=*/false);
-        ImplFillSignaturesBox();
-    }
+        catch ( uno::Exception& )
+        {
+            TOOLS_WARN_EXCEPTION( "xmlsecurity.dialogs", "adding a signature!" );
+            std::unique_ptr<weld::MessageDialog> xBox(Application::CreateMessageDialog(m_xDialog.get(),
+                        VclMessageType::Error, VclButtonsType::Ok,
+                        XsResId(STR_XMLSECDLG_SIGNING_FAILED)));
+            xBox->run();
+            // Don't keep invalid entries...
+            ImplGetSignatureInformations(/*bUseTempStream=*/true, /*bCacheLastSignature=*/false);
+            ImplFillSignaturesBox();
+        }
+    });
 }
 
 IMPL_LINK_NOARG(DigitalSignaturesDialog, RemoveButtonHdl, weld::Button&, void)

OSSII/OxOffice

(cherry picked from commit ced420ca708eb8df5b20c7d537166bd9ec29a0e5)
Conflicts:
	xmlsecurity/source/dialogs/digitalsignaturesdialog.cxx
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/174308
Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

xmlsecurity/source/dialogs/digitalsignaturesdialog.cxx
@@ -530,27 +530,32 @@ IMPL_LINK_NOARG(DigitalSignaturesDialog, AddButtonHdl, weld::Button&, void)
 {
     if( ! canAdd())
         return;
-    try
+    std::vector<uno::Reference<xml::crypto::XXMLSecurityContext>> xSecContexts
     {
-        std::vector<uno::Reference<xml::crypto::XXMLSecurityContext>> xSecContexts
-        {
-            maSignatureManager.getSecurityContext()
-        };
-        // Gpg signing is only possible with ODF >= 1.2 documents
-        if (DocumentSignatureHelper::CanSignWithGPG(maSignatureManager.getStore(), m_sODFVersion))
-            xSecContexts.push_back(maSignatureManager.getGpgSecurityContext());
+        maSignatureManager.getSecurityContext()
+    };
+    // Gpg signing is only possible with ODF >= 1.2 documents
+    if (DocumentSignatureHelper::CanSignWithGPG(maSignatureManager.getStore(), m_sODFVersion))
+        xSecContexts.push_back(maSignatureManager.getGpgSecurityContext());
 
-        std::unique_ptr<CertificateChooser> aChooser = CertificateChooser::getInstance(m_xDialog.get(), m_pViewShell, std::move(xSecContexts), UserAction::Sign);
-        if (aChooser->run() == RET_OK)
+    std::shared_ptr<CertificateChooser> aChooser = CertificateChooser::getInstance(m_xDialog.get(), m_pViewShell, std::move(xSecContexts), UserAction::Sign);
+    aChooser->BeforeRun();
+    weld::DialogController::runAsync(aChooser, [this, aChooser](sal_Int32 nRet) {
+        if (nRet != RET_OK)
+        {
+            return;
+        }
+
+        try
         {
             sal_Int32 nSecurityId;
 
             if (moScriptSignatureManager)
             {
                 if (!moScriptSignatureManager->add(aChooser->GetSelectedCertificates()[0],
-                                                   aChooser->GetSelectedSecurityContext(),
-                                                   aChooser->GetDescription(), nSecurityId,
-                                                   m_bAdESCompliant))
+                            aChooser->GetSelectedSecurityContext(),
+                            aChooser->GetDescription(), nSecurityId,
+                            m_bAdESCompliant))
                 {
                     return;
                 }
@@ -562,7 +567,7 @@ IMPL_LINK_NOARG(DigitalSignaturesDialog, AddButtonHdl, weld::Button&, void)
             }
 
             if (!maSignatureManager.add(aChooser->GetSelectedCertificates()[0], aChooser->GetSelectedSecurityContext(),
-                                        aChooser->GetDescription(), nSecurityId, m_bAdESCompliant))
+                        aChooser->GetDescription(), nSecurityId, m_bAdESCompliant))
                 return;
             mbSignaturesChanged = true;
 
@@ -585,18 +590,18 @@ IMPL_LINK_NOARG(DigitalSignaturesDialog, AddButtonHdl, weld::Button&, void)
                 ImplFillSignaturesBox();
             }
         }
-    }
-    catch ( uno::Exception& )
-    {
-        TOOLS_WARN_EXCEPTION( "xmlsecurity.dialogs", "adding a signature!" );
-        std::unique_ptr<weld::MessageDialog> xBox(Application::CreateMessageDialog(m_xDialog.get(),
-                                                  VclMessageType::Error, VclButtonsType::Ok,
-                                                  XsResId(STR_XMLSECDLG_SIGNING_FAILED)));
-        xBox->run();
-        // Don't keep invalid entries...
-        ImplGetSignatureInformations(/*bUseTempStream=*/true, /*bCacheLastSignature=*/false);
-        ImplFillSignaturesBox();
-    }
+        catch ( uno::Exception& )
+        {
+            TOOLS_WARN_EXCEPTION( "xmlsecurity.dialogs", "adding a signature!" );
+            std::unique_ptr<weld::MessageDialog> xBox(Application::CreateMessageDialog(m_xDialog.get(),
+                        VclMessageType::Error, VclButtonsType::Ok,
+                        XsResId(STR_XMLSECDLG_SIGNING_FAILED)));
+            xBox->run();
+            // Don't keep invalid entries...
+            ImplGetSignatureInformations(/*bUseTempStream=*/true, /*bCacheLastSignature=*/false);
+            ImplFillSignaturesBox();
+        }
+    });
 }
 
 IMPL_LINK_NOARG(DigitalSignaturesDialog, RemoveButtonHdl, weld::Button&, void)