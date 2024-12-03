a8c6d3eb4f0c96b4adce15ce31e73b14a992e4c8
Dev Lo <lodev@ossii.com.tw>
2024-12-03 09:47:46 +0800

    [PATCH] cool#4250 - add API to set per view options.
    Original: Patch 8dcb86a4a724f143f7d5cfcc3d1231a7f924cbd9 from distro/collabora/co-24.04
    by: Michael Meeks <michael.meeks@collabora.com>
    Date: Mon, 18 Nov 2024 13:36:32 +0000
    
    Not applicable due to Patch 4082606aba000a5a13e2ad6b092d280480e04349 from firefly
    [PATCH] [OX] lok: Enhance text input function and provide cursor
    
    Manually added by Dev Lo

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -1358,6 +1358,8 @@ static void doc_postSlideshowCleanup(LibreOfficeKitDocument* pThis);
 static bool doc_renderNextSlideLayer(
     LibreOfficeKitDocument* pThis, unsigned char* pBuffer, bool* bIsBitmapLayer, char** pJsonMsg);
 
+static void doc_setViewOption(LibreOfficeKitDocument* pDoc, const char* pOption, const char* pValue);
+
 static void doc_postWindowExtTextInputEventEnhance(LibreOfficeKitDocument* pThis,
                                                    unsigned nWindowId,
                                                    int nType,
@@ -1568,6 +1570,7 @@ LibLODocument_Impl::LibLODocument_Impl(uno::Reference <css::lang::XComponent> xC
         m_pDocumentClass->createSlideRenderer = doc_createSlideRenderer;
         m_pDocumentClass->postSlideshowCleanup = doc_postSlideshowCleanup;
         m_pDocumentClass->renderNextSlideLayer = doc_renderNextSlideLayer;
+        m_pDocumentClass->setViewOption = doc_setViewOption;
 
         gDocumentClass = m_pDocumentClass;
     }
@@ -5917,6 +5920,11 @@ static bool doc_renderNextSlideLayer(
     *pIsBitmapLayer = bIsBitmapLayer;
 
     return bDone;
+}
+
+static void doc_setViewOption(LibreOfficeKitDocument* /*pDoc*/, const char* /*pOption*/, const char* /*pValue*/)
+{
+    // placeholder for now
 }
 
 static bool getFromTransferable(
