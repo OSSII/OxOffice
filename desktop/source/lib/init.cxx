bb9b0432169e6673030670f5f055ede84bd011b7
Miklos Vajna <vmiklos@collabora.com>
2025-01-02 16:18:03 +0100

    cool#10782 vcl lok: avoid changing the current view during Yield
    Commit 520cc546e2940bdfbc45eab1e569bb06bab17a9c (cool#10782 sfx2 lok:
    fix bad view id on PDF export, 2024-12-20) provided a specific fix for
    one case where spinning the main loop can result in callbacks being
    emitted on the wrong view.
    
    Hunting down all the SfxViewShell::Current() calls and auditing them if
    they are problematic in practice is a large task, so it's desirable to
    handle this problem in a more generic way.
    
    Fix the problem by pushing/popping the current LOK view in
    Application::Reschedule(), which is what e.g. the framework/ status bar
    code uses during PDF export. This keeps the original problem fixed but
    is more generic.
    
    This requires storing function pointers in comphelper/, since normally
    vcl/ code can't call SfxLokHelper, since sfx2/ already depends on vcl/.
    The fix can be tested by reverting the original fix:
    git show 520cc546e2940bdfbc45eab1e569bb06bab17a9c -- sfx2|git apply -R
    and then running the testcase:
    make -C sw -sr CppunitTest_sw_tiledrendering CPPUNIT_TEST_NAME=testPDFExportViewSwitch
    which now passes even if the sfx2-level fix is not there. Keep that
    specific fix, though: the less time we assume SfxViewShell::Current()
    returns the correct view, the better.
    
    Change-Id: Ic1811ff1e67f73fa5066af775d31589136b8502a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/179623
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -8095,6 +8095,7 @@ static void lo_runLoop(LibreOfficeKit* /*pThis*/,
         SolarMutexGuard aGuard;
 
         vcl::lok::registerPollCallbacks(pPollCallback, pWakeCallback, pData);
+        SfxLokHelper::registerViewCallbacks();
         Application::UpdateMainThread();
         soffice_main();
     }
