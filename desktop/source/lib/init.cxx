9a3af960208829da3e7eb3fad373ebfbc94b1399
Miklos Vajna <vmiklos@collabora.com>
2025-01-15 09:32:30 +0100

    cool#10630 doc electronic sign: fix no graphic selection for the signature line
    Insert a signature line in "extern" mode, the shape gets selected but
    there is no graphic selection at a LOK API level.
    
    This is because GetSignPDFCertificate() returned an XCertificate, which
    is empty in the external signing case, so we can't differentiate between
    no signing and external signing.
    
    Fix this by changing the return type to svl::crypto::CertificateOrName,
    this way SdrMarkView::SetMarkHandlesForLOKit() can annotate the
    signature line correctly even in the external signing case.
    
    The tracking of the signature line selection is still in the model (not
    in the view), that's not yet fixed here.
    
    Change-Id: I4ef9c1fa0a88af0c0fcd55156b973a3705f985c0
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180264
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -5299,7 +5299,7 @@ static bool isCommandAllowed(OUString& command) {
                 {
                     // If the just added signature line shape is selected, allow moving it.
                     SfxObjectShell* pDocShell = pViewShell->GetObjectShell();
-                    bRet = pDocShell->GetSignPDFCertificate().is();
+                    bRet = pDocShell->GetSignPDFCertificate().Is();
                 }
                 return bRet;
             }
