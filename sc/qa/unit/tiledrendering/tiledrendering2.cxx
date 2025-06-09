LibreOffice/core

cool#11475 lok: sc: correctly export frozen/split pane information
As 7b4802070ac6fb930255536bf3ed2c52428b4181 noted,
online only supports freeze panes, but preserves splits when saving.

However, files could also end up with mixed frozen/split panes.
In addition, even a single split was treated like two, with
the other split being at the first row/column, which was incorrect.

Change-Id: I974690b8877ad6d4eca1e046c3c352359a199706
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/184421
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

sc/qa/unit/tiledrendering/tiledrendering2.cxx
@@ -165,6 +165,42 @@ CPPUNIT_TEST_FIXTURE(ScTiledRenderingTest, testCool11739LocaleDialogFieldUnit)
     CPPUNIT_ASSERT_EQUAL(FieldUnit::CM, eMetric);
 }
 
+CPPUNIT_TEST_FIXTURE(ScTiledRenderingTest, testSplitPanes)
+{
+    createDoc("split-panes.ods");
+
+    save(u"calc8"_ustr);
+
+    xmlDocUniquePtr pSettings = parseExport(u"settings.xml"_ustr);
+    CPPUNIT_ASSERT(pSettings);
+
+    // Without the fix in place, this test would have failed with
+    // - Expected: 0
+    // - Actual  : 2
+    assertXPathContent(pSettings,
+                       "/office:document-settings/office:settings/config:config-item-set[1]/"
+                       "config:config-item-map-indexed/config:config-item-map-entry/"
+                       "config:config-item-map-named/config:config-item-map-entry/"
+                       "config:config-item[@config:name='VerticalSplitMode']",
+                       u"0");
+}
+
+CPPUNIT_TEST_FIXTURE(ScTiledRenderingTest, testSplitPanesXLSX)
+{
+    createDoc("split-panes.xlsx");
+
+    save(u"Calc Office Open XML"_ustr);
+
+    xmlDocUniquePtr pSheet = parseExport(u"xl/worksheets/sheet1.xml"_ustr);
+    CPPUNIT_ASSERT(pSheet);
+
+    // Without the fix in place, this test would have failed with
+    // - Expected: topRight
+    // - Actual  : bottomRight
+    // which also results in invalid XLSX
+    assertXPath(pSheet, "/x:worksheet/x:sheetViews/x:sheetView/x:pane", "activePane", u"topRight");
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

OSSII/OxOffice

(cherry picked from commit 7b8718414158b2ff2b7985b275d2c4842f1ac3f8)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/184869

sc/qa/unit/tiledrendering/tiledrendering2.cxx
@@ -222,6 +222,43 @@ CPPUNIT_TEST_FIXTURE(Test, testCool11739LocaleDialogFieldUnit)
 }
 }
 
+CPPUNIT_TEST_FIXTURE(Test, testSplitPanes)
+{
+    createDoc("split-panes.ods");
+
+    save("calc8");
+
+    xmlDocUniquePtr pSettings = parseExport("settings.xml");
+    CPPUNIT_ASSERT(pSettings);
+
+    // Without the fix in place, this test would have failed with
+    // - Expected: 0
+    // - Actual  : 2
+    assertXPathContent(pSettings,
+                       "/office:document-settings/office:settings/config:config-item-set[1]/"
+                       "config:config-item-map-indexed/config:config-item-map-entry/"
+                       "config:config-item-map-named/config:config-item-map-entry/"
+                       "config:config-item[@config:name='VerticalSplitMode']"_ostr,
+                       u"0"_ustr);
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testSplitPanesXLSX)
+{
+    createDoc("split-panes.xlsx");
+
+    save("Calc Office Open XML");
+
+    xmlDocUniquePtr pSheet = parseExport("xl/worksheets/sheet1.xml");
+    CPPUNIT_ASSERT(pSheet);
+
+    // Without the fix in place, this test would have failed with
+    // - Expected: topRight
+    // - Actual  : bottomRight
+    // which also results in invalid XLSX
+    assertXPath(pSheet, "/x:worksheet/x:sheetViews/x:sheetView/x:pane"_ostr, "activePane"_ostr,
+                u"topRight"_ustr);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */