d66affed64a49e9a5a950c707d5fe1a124926583
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-05-06 15:37:30 +0100

    check for hb_shape_full failure
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I5fafcb7d592fb4c9091cb2bab64cb6ac47462d71
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/2104
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

--- vcl/source/gdi/CommonSalLayout.cxx
+++ vcl/source/gdi/CommonSalLayout.cxx
@@ -429,10 +429,11 @@ bool GenericSalLayout::LayoutText(vcl::text::ImplLayoutArgs& rArgs,
             // The shapers that we want HarfBuzz to use, in the order of
             // preference.
             const char* const pHbShapers[] = { "graphite2", "ot", "fallback", nullptr };
-            bool ok = hb_shape_full(pHbFont, pHbBuffer, maFeatures.data(), maFeatures.size(),
-                                    pHbShapers);
-            assert(ok);
-            (void)ok;
+            if (!hb_shape_full(pHbFont, pHbBuffer, maFeatures.data(), maFeatures.size(), pHbShapers))
+            {
+                SAL_WARN("vcl.harfbuzz", "hb_shape_full failed");
+                hb_buffer_set_length(pHbBuffer, 0);
+            }
 
             int nRunGlyphCount = hb_buffer_get_length(pHbBuffer);
             hb_glyph_info_t* pHbGlyphInfos = hb_buffer_get_glyph_infos(pHbBuffer, nullptr);
